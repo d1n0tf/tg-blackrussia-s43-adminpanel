f2fad340deeb2a1c06e91ddb87cb68c6afa7084c
d1n0tf <ilushkaplyshka@gmail.com>
2026-06-20 03:04:55 +0400

    update

--- main.py
+++ main.py
@@ -14,6 +14,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host=os.getenv("HOST", "0.0.0.0"),
-        port=int(os.getenv("PORT", "6000")),
+        port=int(os.getenv("PORT", "8000")),
         reload=os.getenv("RELOAD") == "1",
     )
